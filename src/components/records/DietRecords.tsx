79e6ac69dd9420b4ef28dca0081a45bc7849dac7
Jeff-Hsu024 <hsu024722@gmail.com>
2025-06-26 04:34:54 +0000

    adjust record ui

--- src/components/records/DietRecords.tsx
+++ src/components/records/DietRecords.tsx
@@ -53,7 +53,6 @@ function DietRecords() {
   };
 
   return (
-
     <div className="tabs tabs-lift">
       <label className="tab">
         <input type="radio" name="my_tabs_4" defaultChecked />
@@ -63,7 +62,6 @@ function DietRecords() {
       <div className="tab-content bg-base-100 border-base-300 p-6">
         <Charts />
       </div>
-
       <label className="tab">
         <input type="radio" name="my_tabs_4" />
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 me-2"><path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" /></svg>
@@ -98,13 +96,6 @@ function DietRecords() {
           <Pagination pageCount={pageCount} onPageChange={handlePageClick} />
         </div>
       </div>
-
-      <label className="tab">
-        <input type="radio" name="my_tabs_4" />
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-4 me-2"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
-        Love
-      </label>
-      <div className="tab-content bg-base-100 border-base-300 p-6">Tab content 3</div>
     </div>
   );
 }
