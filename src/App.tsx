ad70de65916a933370cc4b0d583826eafea90175
Jeff-Hsu024 <hsu024722@gmail.com>
2025-06-26 01:28:37 +0000

    empty codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
   return (
     <>
-      <Header />
-      <MainContent>
-        <Dashboard />
-      </MainContent>
-      <Footer />
+
     </>
   );
 }
